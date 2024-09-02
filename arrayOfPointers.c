2b04e7261eabf960ca053755e6da21dfc29c5433
Dylan Wilkins <dylanwilkins2308@example.com>
2024-09-02 17:13:01 -0400

    - defines & includes

---
+++ arrayOfPointers.c
@@ -0,0 +1,10 @@
+// test code for dynamic memory allocation of array of pointers to structs
+// week 1 review for cs1 (data structs & alg.)
+
+#include <stdio.h>
+#include <time.h>
+
+#define ABSOLUTE_MAX 100000
+#define MAX_INT 32767
+#define DEFAULT 10
+
